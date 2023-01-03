5af89f9d530393daa0d7a12948c8e864e34d5ef5
baarbarabottazzi <barbiiee.8@gmail.com>
2023-01-03 15:47:22 -0300

    connection cnf

--- models/connection.js
+++ models/connection.js
@@ -1,21 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2")
 
 // create the connection to database
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || " ",
-    database: process.env.DB_DATABASE || "productos",
-    port: process.env.DB_PORT || 3306
-});
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || " ",
+  database: process.env.DB_DATABASE || "productos",
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
 
-
-connection.connect();
+/* connection.connect()
 // truco para mantener la conexión
-setInterval(function ()
-{
-    connection.query('SELECT 1');
-    // console.log("manteniendo viva la conexion")
-}, 50000);
-
-module.exports = connection;
+setInterval(function () {
+  connection.query("SELECT 1")
+  // console.log("manteniendo viva la conexion")
+}, 50000)
+*/
+module.exports = pool
--- server.js
+++ server.js
@@ -1,45 +1,44 @@
-const express = require("express");
-const app = express();
-const hbs = require("hbs");
-require("./views/helpers/helpers.js");
+const express = require("express")
+const app = express()
+const hbs = require("hbs")
+require("./views/helpers/helpers.js")
 //modulo nativo
-const path = require("path");
-const rutasBack = require("./routes/back.js");
-const rutasFront = require("./routes/front.js");
-const session = require("express-session");
-const port = process.env.PORT || 3000;
-//sesiones mediante cookies 
-app.use(session({
-  secret: "sarasa",
-  resave: true,
-  saveUninitialized: false,
-  cookie: { maxAge: 300000 } // 5 minutos
-
-}));
+const path = require("path")
+const rutasBack = require("./routes/back.js")
+const rutasFront = require("./routes/front.js")
+const session = require("express-session")
+const port = process.env.PORT || 3000
+//sesiones mediante cookies
+app.use(
+  session({
+    secret: "sarasa",
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 300000 }, // 5 minutos
+  })
+)
 
 //middleware para tomar datos form
-app.use(express.json());
+app.use(express.json())
 app.use(
   express.urlencoded({
     extended: false,
   })
-);
-// Fija una ruta estatica 
-app.use(express.static("public"));
-app.set("view engine", "hbs");
+)
+// Fija una ruta estatica
+app.use(express.static("public"))
+app.set("view engine", "hbs")
 app.set("views", [
   path.join("./views/front"),
   path.join("./views/back"),
   path.join("./views"),
-]);
-hbs.registerPartials(__dirname + "/views/partials"); // indicamos el directorio hacia los parciales
+])
+hbs.registerPartials(__dirname + "/views/partials") // indicamos el directorio hacia los parciales
 
-app.use("/", rutasFront);
-app.use("/", rutasBack);
-app.use((req, res, next) =>
-{
-  res.status(404).render("404");
-});
+app.use("/", rutasFront)
+app.use("/", rutasBack)
+app.use((req, res, next) => {
+  res.status(404).render("404")
+})
 
-
-app.listen(port);
+app.listen(port)
